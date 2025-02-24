edb20c7ed0e90a2affce97eca286994f21cd4e12
miscakarza <miscakarza@gmail.com>
2025-02-25 04:12:27 +0700

    add pinging server every 5 mins

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const path = require("path");
 const pageRoute = require("./routes/pageRoute");
 const bodyParser = require("body-parser");
+const cron = require("node-cron");
+const axios = require("axios");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,4 +17,15 @@ app.use("/", pageRoute);
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// Cron job to ping the server every 5 minutes to keep it warm
+cron.schedule("*/5 * * * *", async () => {
+  try {
+    const serverUrl =
+      `https://kylongtech.azurewebsites.net/main` || `http://localhost:${PORT}`;
+    await axios.get(serverUrl);
+  } catch (error) {
+    console.error("Failed to ping server:", error.message);
+  }
 });
