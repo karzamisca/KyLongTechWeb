6e4c93bde7206a35ff2674b45bdb23ce00a39adf
miscakarza <miscakarza@gmail.com>
2025-03-10 20:00:52 +0700

    eliminate admin page and its routes and controllers, downside node modules

--- app.js
+++ app.js
@@ -3,34 +3,17 @@ const express = require("express");
 const path = require("path");
 const pageRoute = require("./routes/pageRoute");
 const bodyParser = require("body-parser");
-const cron = require("node-cron");
-const axios = require("axios");
-const authRoute = require("./routes/authRoute");
-const fileRoute = require("./routes/fileRoute");
 const cookieParser = require("cookie-parser");
-const connectDB = require("./config/db");
-const authMiddleware = require("./middlewares/authMiddleware"); // JWT middleware
 require("dotenv").config();
 const app = express();
-connectDB();
 const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static("views"));
-app.use(
-  "/thumbnails",
-  express.static(path.join(__dirname, "views/homePage/thumbnails"))
-);
-app.use("/pdfs", express.static(path.join(__dirname, "views/homePage/pdf")));
 
-// Public routes
-app.use("/", authRoute);
-app.use("/", pageRoute); // All routes in pageRoute are now public
-
-// Protected routes
-app.use("/", authMiddleware, fileRoute);
+app.use("/", pageRoute);
 
 // Start the server
 app.listen(PORT, () => {
