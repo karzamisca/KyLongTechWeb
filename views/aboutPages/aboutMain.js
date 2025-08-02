f93ad91c3d3a37c6a71a72f0c72e0700678081fa
karzamisca <miscakarza@gmail.com>
2025-08-02 13:53:50 +0700

    component header: remake

--- views/aboutPages/aboutMain.js
+++ views/aboutPages/aboutMain.js
@@ -1,12 +1,3 @@
-// Load header
-fetch("components/header/header.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById("header").innerHTML = data;
-    // Set about as active
-    document.getElementById("about").classList.add("active");
-  });
-
 // Load footer
 fetch("components/footer/footer.html")
   .then((response) => response.text())
--- views/components/header/header.js
+++
@@ -1,9 +0,0 @@
-////views\components\header\header.js
-// This script automatically highlights the active navigation link
-fetch("components/header/header.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById("header").innerHTML = data;
-
-    // After header is loaded, run the navigation highlighting code
-  });
